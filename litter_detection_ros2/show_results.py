a8f57e5ba3bdcf3991ab63b863a301d2830c2aa0
Guido Lazzerini <guido.lazzerini@unifi.it>
2024-10-03 09:54:45 +0000

    show results reads detection_data topic and  uses opencv to viualize results

--- litter_detection_ros2/show_results.py
+++ litter_detection_ros2/show_results.py
@@ -1,25 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import CompressedImage
 from custom_msgs.msg import DetectionResults
-from cv_bridge import CvBridge
 import numpy as np
 import cv2
+
 
 class ImageProcessorNode(Node):
     def __init__(self):
         super().__init__('image_processor_node')
 
-        # Publishers
-        self.detection_pub = self.create_publisher(CompressedImage, '/detection', 1)
-
-        self.get_logger().error("here")
-
         # Subscribers
         self.detection_data_sub = self.create_subscription(DetectionResults, '/detection_data', self.listener_callback, 1)
 
-        # CvBridge to convert ROS Image messages to OpenCV format
-        self.bridge = CvBridge()
+        # Counter to save images with unique names
+        self.image_counter = 0
 
     def listener_callback(self, msg: DetectionResults):
 
@@ -27,7 +21,6 @@ class ImageProcessorNode(Node):
 
         # Decompress the image using OpenCV
         cv_image = cv2.imdecode(compressed_image, cv2.IMREAD_COLOR)
-
 
         # Check if image was successfully decompressed
         if cv_image is None:
@@ -46,14 +39,21 @@ class ImageProcessorNode(Node):
             ymax = int(msg.box_coordinates[i * 4 + 3])
 
             # Draw the bounding box
-            cv2.rectangle(cv_image, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+            cv2.rectangle(cv_image, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
 
             # Draw the label and confidence
             label_text = f'{label}: {confidence:.2f}'
-            cv2.putText(cv_image, label_text, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            cv2.putText(cv_image, label_text, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-            self.detection_pub.publish(self.bridge.cv2_to_compressed_imgmsg(cv_image, "jpg"))
+        # Use the counter to create a unique filename
+        filename = f'/home/guidolazzerini/ros_data/litter_detection/frames/bts/prova_{self.image_counter}.png'
+        self.image_counter += 1  # Increment the counter for the next image
 
+        # Save the image with the unique filename
+        # cv2.imwrite(filename, cv_image)
+
+        cv2.imshow('Image', cv_image)
+        cv2.waitKey(1)
 
 
 def main(args=None):
